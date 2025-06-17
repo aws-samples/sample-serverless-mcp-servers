388d460edd0c967904efb8efa3f14423afe3410f
heeki <heekipark@gmail.com>
2025-06-17 17:26:19 -0400

    chore(stateless-mcp-on-lambda-python): updated naming for consistency

--- stateless-mcp-on-lambda-python/etc/environment.sh
+++ stateless-mcp-on-lambda-python/etc/environment.sh
@@ -5,7 +5,7 @@ REGION=us-east-1
 
 # mcp dependencies
 P_DESCRIPTION="mcp==1.8.0"
-LAYER_STACK=mcp-lambda-layer
+LAYER_STACK=samples-mcp-lambda-layer
 LAYER_TEMPLATE=sam/layer.yaml
 LAYER_OUTPUT=sam/layer_output.yaml
 LAYER_PARAMS="ParameterKey=description,ParameterValue=${P_DESCRIPTION}"
@@ -15,7 +15,7 @@ O_LAYER_ARN=your-output-layer-arn
 P_API_STAGE=dev
 P_FN_MEMORY=128
 P_FN_TIMEOUT=15
-APIGW_STACK=mcp-apigw
+APIGW_STACK=samples-mcp-apigw
 APIGW_TEMPLATE=sam/template.yaml
 APIGW_OUTPUT=sam/template_output.yaml
 APIGW_PARAMS="ParameterKey=apiStage,ParameterValue=${P_API_STAGE} ParameterKey=fnMemory,ParameterValue=${P_FN_MEMORY} ParameterKey=fnTimeout,ParameterValue=${P_FN_TIMEOUT} ParameterKey=dependencies,ParameterValue=${O_LAYER_ARN}"
