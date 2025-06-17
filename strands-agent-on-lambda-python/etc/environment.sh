f320011486ca2b91101735cf366a197c06e756d2
heeki <heekipark@gmail.com>
2025-06-17 17:28:02 -0400

    chore(strands-agent-on-lambda-python): updated naming for consistency

--- strands-agent-on-lambda-python/etc/environment.sh
+++ strands-agent-on-lambda-python/etc/environment.sh
@@ -5,7 +5,7 @@ REGION=us-east-1
 
 # mcp dependencies
 P_DESCRIPTION="strands-agents==0.0.1"
-LAYER_STACK=sample-strands-agents-lambda-layer
+LAYER_STACK=samples-strands-agents-lambda-layer
 LAYER_TEMPLATE=sam/layer.yaml
 LAYER_OUTPUT=sam/layer_output.yaml
 LAYER_PARAMS="ParameterKey=description,ParameterValue=${P_DESCRIPTION}"
@@ -15,7 +15,7 @@ O_LAYER_ARN=output-layer-arn
 P_API_STAGE=dev
 P_FN_MEMORY=128
 P_FN_TIMEOUT=60
-APIGW_STACK=sample-strands-agents-apigw
+APIGW_STACK=samples-strands-agents-apigw
 APIGW_TEMPLATE=sam/template.yaml
 APIGW_OUTPUT=sam/template_output.yaml
 APIGW_PARAMS="ParameterKey=apiStage,ParameterValue=${P_API_STAGE} ParameterKey=fnMemory,ParameterValue=${P_FN_MEMORY} ParameterKey=fnTimeout,ParameterValue=${P_FN_TIMEOUT} ParameterKey=dependencies,ParameterValue=${O_LAYER_ARN}"
