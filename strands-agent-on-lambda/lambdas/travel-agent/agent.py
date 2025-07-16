7e6d248e4f747d2da5cf9c9ef48c791fa2a24828
aal80 <anton.al80@gmail.com>
2025-07-16 18:19:47 -0500

    Updated code + terraform to use S3SessionManager

--- strands-agent-on-lambda/lambdas/travel-agent/agent.py
+++ strands-agent-on-lambda/lambdas/travel-agent/agent.py
@@ -1,16 +1,42 @@
-from strands.types.exceptions import MCPClientInitializationError
-
-import agent_state_manager
+from strands import Agent
+from strands.session.s3_session_manager import S3SessionManager
+import os
 import logging
 from user import User
+import mcp_client_manager
+import tools
+from agent_config import model, system_prompt
+
 l = logging.getLogger(__name__)
 l.setLevel(logging.INFO)
 
-def prompt(user: User, text: str):
+SESSION_STORE_BUCKET_NAME = os.environ['SESSION_STORE_BUCKET_NAME']
+l.info(f"SESSION_STORE_BUCKET_NAME={SESSION_STORE_BUCKET_NAME}")
+
+def prompt(user: User, composite_prompt: str):
     l.info(f"user.id={user.id}, user.name={user.name}")
 
+    session_manager = S3SessionManager(
+        session_id=f"session_for_user_{user.id}",
+        bucket=SESSION_STORE_BUCKET_NAME,
+        prefix="agent_sessions"
+    )
+
     try:
-        agent = agent_state_manager.restore(user)
+        mcp_tools = mcp_client_manager.get_mcp_tools_for_user(user)
+        agent = Agent(
+            model=model,
+            agent_id="travel_agent",
+            session_manager=session_manager,
+            system_prompt=system_prompt,
+            callback_handler=None,
+            tools=[tools] + mcp_tools,
+
+        )
+        agent_response = agent(composite_prompt)
+        response_text = agent_response.message["content"][0]["text"]
+        return response_text
+
     except Exception as e:
 
         l.info(type(e))
@@ -18,8 +44,3 @@ def prompt(user: User, text: str):
         # l.error(e)
         return 'Failed to initialize MCP Client, see logs'
 
-    agent_response = agent(text)
-
-    agent_state_manager.save(user, agent)
-    response_text = agent_response.message["content"][0]["text"]
-    return response_text
