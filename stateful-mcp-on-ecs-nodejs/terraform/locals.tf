a07a09c46d6718c3884cf4597b6574095e607d6d
aal80 <anton.al80@gmail.com>
2025-08-06 22:33:30 -0500

    updated ecs mcps to latest mcp sdk

--- stateful-mcp-on-ecs-nodejs/terraform/locals.tf
+++ stateful-mcp-on-ecs-nodejs/terraform/locals.tf
@@ -7,7 +7,7 @@ locals {
   subnet1_cidr             = "10.0.1.0/24"
   subnet2_cidr             = "10.0.2.0/24"
   ecs_task_container_name  = "mcp_server"
-  ecs_task_container_image = "public.ecr.aws/antonaws/stateful-mcp-on-ecs:latest"
+  ecs_task_container_image = "public.ecr.aws/your-ecr-alias/stateful-mcp-on-ecs:latest"
   ecs_task_container_port  = 3000
   # r53_zone_name            = "your-zone-name"
 }
